683478bb764a201b62d11e5664e4b51283975ed9
RLeventisB <pipipe2006@gmail.com>
2024-08-18 20:12:38 -0400

    ink color fixes now ink color aliases loads first than ink color tags since in some update the data loading thing became asynchronous some fixes for organic ink being called dye ink still

--- src/main/java/net/splatcraft/forge/data/InkColorAliases.java
+++ src/main/java/net/splatcraft/forge/data/InkColorAliases.java
@@ -127,6 +127,7 @@ public class InkColorAliases
                     }
                 }
             }
+            InkColorTags.Listener.doLoad();
         }
     }
 }
--- src/main/java/net/splatcraft/forge/handlers/DataHandler.java
+++ src/main/java/net/splatcraft/forge/handlers/DataHandler.java
@@ -4,9 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.mojang.serialization.JsonOps;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
@@ -20,89 +17,87 @@ import net.minecraftforge.fml.common.Mod;
 import net.splatcraft.forge.Splatcraft;
 import net.splatcraft.forge.data.InkColorAliases;
 import net.splatcraft.forge.data.InkColorTags;
-import net.splatcraft.forge.items.weapons.settings.AbstractWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.BlasterWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.ChargerWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.DualieWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.RollerWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.ShooterWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.SlosherWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.SplatlingWeaponSettings;
-import net.splatcraft.forge.items.weapons.settings.SubWeaponSettings;
+import net.splatcraft.forge.items.weapons.settings.*;
 import net.splatcraft.forge.network.SplatcraftPacketHandler;
 import net.splatcraft.forge.network.s2c.UpdateWeaponSettingsPacket;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Mod.EventBusSubscriber
 public class DataHandler
 {
-	public static final WeaponStatsListener WEAPON_STATS_LISTENER = new WeaponStatsListener();
-	public static final InkColorTags.Listener INK_COLOR_TAGS_LISTENER = new InkColorTags.Listener();
-	public static final InkColorAliases.Listener INK_COLOR_ALIASES_LISTENER = new InkColorAliases.Listener();
-	@SubscribeEvent
-	public static void addReloadListener(AddReloadListenerEvent event)
-	{
-		event.addListener(WEAPON_STATS_LISTENER);
-		event.addListener(INK_COLOR_TAGS_LISTENER);
-		event.addListener(INK_COLOR_ALIASES_LISTENER);
-	}
+    public static final WeaponStatsListener WEAPON_STATS_LISTENER = new WeaponStatsListener();
+    public static final InkColorTags.Listener INK_COLOR_TAGS_LISTENER = new InkColorTags.Listener();
+    public static final InkColorAliases.Listener INK_COLOR_ALIASES_LISTENER = new InkColorAliases.Listener();
 
-	@SubscribeEvent
-	public static void onDataSync(OnDatapackSyncEvent event)
-	{
-		SplatcraftPacketHandler.sendToAll(new UpdateWeaponSettingsPacket());
-	}
+    @SubscribeEvent
+    public static void addReloadListener(AddReloadListenerEvent event)
+    {
+        event.addListener(WEAPON_STATS_LISTENER);
+        event.addListener(INK_COLOR_TAGS_LISTENER);
+        event.addListener(INK_COLOR_ALIASES_LISTENER);
+    }
 
-	public static class WeaponStatsListener extends SimpleJsonResourceReloadListener
-	{
-		public static final HashMap<String, Class<? extends AbstractWeaponSettings<?, ?>>> SETTING_TYPES = new HashMap<>()
-		{{
-			put(Splatcraft.MODID + ":shooter", ShooterWeaponSettings.class);
-			put(Splatcraft.MODID + ":blaster", BlasterWeaponSettings.class);
-			put(Splatcraft.MODID + ":roller", RollerWeaponSettings.class);
-			put(Splatcraft.MODID + ":charger", ChargerWeaponSettings.class);
-			put(Splatcraft.MODID + ":slosher", SlosherWeaponSettings.class);
-			put(Splatcraft.MODID + ":dualie", DualieWeaponSettings.class);
-			put(Splatcraft.MODID + ":splatling", SplatlingWeaponSettings.class);
-			put(Splatcraft.MODID + ":sub_weapon", SubWeaponSettings.class);
-		}}; //TODO make better registry probably
-		public static final HashMap<ResourceLocation, AbstractWeaponSettings<?, ?>> SETTINGS = new HashMap<>();
-		private static final Gson GSON_INSTANCE = Deserializers.createFunctionSerializer().create();
-		private static final String folder = "weapon_settings";
-		public WeaponStatsListener()
-		{
-			super(GSON_INSTANCE, folder);
-		}
+    @SubscribeEvent
+    public static void onDataSync(OnDatapackSyncEvent event)
+    {
+        SplatcraftPacketHandler.sendToAll(new UpdateWeaponSettingsPacket());
+    }
+    
+    public static class WeaponStatsListener extends SimpleJsonResourceReloadListener
+    {
+        public static final HashMap<String, Class<? extends AbstractWeaponSettings<?, ?>>> SETTING_TYPES = new HashMap<>()
+        {{
+            put(Splatcraft.MODID + ":shooter", ShooterWeaponSettings.class);
+            put(Splatcraft.MODID + ":blaster", BlasterWeaponSettings.class);
+            put(Splatcraft.MODID + ":roller", RollerWeaponSettings.class);
+            put(Splatcraft.MODID + ":charger", ChargerWeaponSettings.class);
+            put(Splatcraft.MODID + ":slosher", SlosherWeaponSettings.class);
+            put(Splatcraft.MODID + ":dualie", DualieWeaponSettings.class);
+            put(Splatcraft.MODID + ":splatling", SplatlingWeaponSettings.class);
+            put(Splatcraft.MODID + ":sub_weapon", SubWeaponSettings.class);
+        }}; //TODO make better registry probably
+        public static final HashMap<ResourceLocation, AbstractWeaponSettings<?, ?>> SETTINGS = new HashMap<>();
+        private static final Gson GSON_INSTANCE = Deserializers.createFunctionSerializer().create();
+        private static final String folder = "weapon_settings";
 
-		@Override
-		protected void apply(Map<ResourceLocation, JsonElement> resourceList, @NotNull ResourceManager resourceManagerIn, @NotNull ProfilerFiller profilerIn)
-		{
-			SETTINGS.clear();
+        public WeaponStatsListener()
+        {
+            super(GSON_INSTANCE, folder);
+        }
 
-			resourceList.forEach((key, element) ->
-			{
-				JsonObject json = element.getAsJsonObject();
-				try
-				{
-					String type = GsonHelper.getAsString(json, "type");
+        @Override
+        protected void apply(Map<ResourceLocation, JsonElement> resourceList, @NotNull ResourceManager resourceManagerIn, @NotNull ProfilerFiller profilerIn)
+        {
+            SETTINGS.clear();
 
-					if (!SETTING_TYPES.containsKey(type))
-						return;
+            resourceList.forEach((key, element) ->
+            {
+                JsonObject json = element.getAsJsonObject();
+                try
+                {
+                    String type = GsonHelper.getAsString(json, "type");
 
-					AbstractWeaponSettings<?, ?> settings = SETTING_TYPES.get(type).getConstructor(String.class).newInstance(key.toString());
-					settings.getCodec().parse(JsonOps.INSTANCE, json).resultOrPartial(msg -> Splatcraft.LOGGER.error("Failed to load weapon settings for %s: %s".formatted(key, msg))).ifPresent(
-						settings::castAndDeserialize
-					);
+                    if (!SETTING_TYPES.containsKey(type))
+                        return;
 
-					settings.registerStatTooltips();
-					SETTINGS.put(key, settings);
-				}
-				catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-				       NoSuchMethodException e)
-				{
-					throw new RuntimeException(e);
-				}
-			});
-		}
-	}
+                    AbstractWeaponSettings<?, ?> settings = SETTING_TYPES.get(type).getConstructor(String.class).newInstance(key.toString());
+                    settings.getCodec().parse(JsonOps.INSTANCE, json).resultOrPartial(msg -> Splatcraft.LOGGER.error("Failed to load weapon settings for %s: %s".formatted(key, msg))).ifPresent(
+                            settings::castAndDeserialize
+                    );
+
+                    settings.registerStatTooltips();
+                    SETTINGS.put(key, settings);
+                }
+                catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                       NoSuchMethodException e)
+                {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
 }
