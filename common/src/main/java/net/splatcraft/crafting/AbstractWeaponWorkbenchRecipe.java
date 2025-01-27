8a9a51027661e2974a153ed3547847326a59eb70
RLeventisB <pipipe2006@gmail.com>
2025-01-27 20:03:57 -0300

    no you use more than one slot

--- common/src/main/java/net/splatcraft/crafting/AbstractWeaponWorkbenchRecipe.java
+++ common/src/main/java/net/splatcraft/crafting/AbstractWeaponWorkbenchRecipe.java
@@ -4,7 +4,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.RecipeType;
-import net.minecraft.recipe.input.SingleStackRecipeInput;
+import net.minecraft.recipe.input.RecipeInput;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class AbstractWeaponWorkbenchRecipe implements Recipe<SingleStackRecipeInput>
+public abstract class AbstractWeaponWorkbenchRecipe implements Recipe<RecipeInput>
 {
     protected final ItemStack recipeOutput;
     protected final List<StackedIngredient> recipeItems;
@@ -28,7 +28,7 @@ public abstract class AbstractWeaponWorkbenchRecipe implements Recipe<SingleStac
     }
 
     @Override
-    public boolean matches(SingleStackRecipeInput inv, @NotNull World world)
+    public boolean matches(RecipeInput inv, @NotNull World world)
     {
         List<ItemStack> inputs = new ArrayList<>();
         int i = 0;
@@ -52,7 +52,7 @@ public abstract class AbstractWeaponWorkbenchRecipe implements Recipe<SingleStac
     }
 
     @Override
-    public @NotNull ItemStack craft(@NotNull SingleStackRecipeInput inv, @NotNull RegistryWrapper.WrapperLookup access)
+    public @NotNull ItemStack craft(@NotNull RecipeInput inv, @NotNull RegistryWrapper.WrapperLookup access)
     {
         return recipeOutput;
     }
