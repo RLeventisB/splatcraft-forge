7c48211b5e367d7edc5954d62ce32e7b4be2a3a4
RLeventisB <pipipe2006@gmail.com>
2025-06-26 23:12:28 -0400

    fixed being able to scroll out of an locked slot (for example when shooting splatlings) fixed ballpoint having good stats because i totally didnt copy it from heavy splatling

--- common/src/main/java/net/splatcraft/mixin/CooldownSlotMixin.java
+++ common/src/main/java/net/splatcraft/mixin/CooldownSlotMixin.java
@@ -3,36 +3,63 @@ package net.splatcraft.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
 import net.splatcraft.client.handlers.PlayerSlotHandler;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
 public class CooldownSlotMixin
 {
-	@Shadow
-	@Nullable
-	public LocalPlayer player;
-	@Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;render(Lnet/minecraft/client/DeltaTracker;Z)V", shift = At.Shift.AFTER))
-	public void splatcraft$onRenderEnd(boolean tick, CallbackInfo ci)
+	@Mixin(Minecraft.class)
+	public static class MinecraftPart
 	{
-		PlayerSlotHandler.reassignSlot((Minecraft) (Object) this);
-	}
-	@WrapOperation(method = "handleKeybinds", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Inventory;selected:I"))
-	public void splatcraft$onHotbarSlotChange(Inventory instance, int value, Operation<Void> original)
-	{
-		int slot = PlayerSlotHandler.slotToAssign(player);
-		if (slot != -1)
+		@Shadow
+		@Nullable
+		public LocalPlayer player;
+		@Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GameRenderer;render(Lnet/minecraft/client/DeltaTracker;Z)V", shift = At.Shift.AFTER))
+		public void splatcraft$onRenderEnd(boolean tick, CallbackInfo ci)
 		{
-			PlayerSlotHandler.enqueueSlot(value);
-			value = slot;
+			PlayerSlotHandler.reassignSlot((Minecraft) (Object) this);
 		}
-		original.call(instance, value);
+		@WrapOperation(method = "handleKeybinds", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Inventory;selected:I"))
+		public void splatcraft$onHotbarSlotChange(Inventory instance, int value, Operation<Void> original)
+		{
+			int slot = PlayerSlotHandler.slotToAssign(player);
+			if (slot != -1)
+			{
+				PlayerSlotHandler.enqueueSlot(value);
+				value = slot;
+			}
+			original.call(instance, value);
+		}
+	}
+	@Mixin(MouseHandler.class)
+	public static class MouseHandlerPart
+	{
+		@Shadow
+		@Final
+		private Minecraft minecraft;
+		@WrapOperation(method = "onScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;swapPaint(D)V"))
+		public void splatcraft$onHotbarSlotScroll(Inventory instance, double direction, Operation<Void> original)
+		{
+			int slot = PlayerSlotHandler.slotToAssign(minecraft.player);
+			int currentSlot = minecraft.player.getInventory().selected;
+			int nextSlot = (int) Mth.positiveModulo(currentSlot + direction, 9);
+			if (slot != -1)
+			{
+				PlayerSlotHandler.enqueueSlot(nextSlot);
+				minecraft.player.getInventory().selected = slot;
+				return;
+			}
+			original.call(instance, direction);
+		}
 	}
 }
